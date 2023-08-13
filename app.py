48e5a00222efe8f1e785b4de836aa2824a7d99a3
Amanda Souza <amandasilvadsz@gmail.com>
2023-08-13 12:13:49 -0300

    On branch main 	modified:   app.py 	modified:   instance/database.db 	modified:   model/__pycache__/models.cpython-311.pyc 	modified:   model/__pycache__/resources.cpython-311.pyc 	modified:   model/models.py 	modified:   model/resources.py 	new file:   requirements.txt

--- app.py
+++ app.py
@@ -1,8 +1,7 @@
 #app.py 
-
 from flask import Flask
 from flask_restful import Api
-from model.models import db
+from model.models import db, ma
 from model.resources import TutorResource, PetResource
 
 
@@ -10,12 +9,13 @@ app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
 api = Api(app)
 db.init_app(app)
+ma.init_app(app)
 
 with app.app_context():
   db.create_all()
   
-api.add_resource(TutorResource, '/tutor/<int:tutor_id>', '/atualizarTutor/<int:tutor_id>')
-api.add_resource(PetResource, '/pet/<int:pet_id>', '/atualizarPet/<int:pet_id>')
+api.add_resource(TutorResource, '/tutor', '/tutor/<int:tutor_id>')
+api.add_resource(PetResource, '/pet', '/pet/<int:pet_id>')
 
 if __name__ == '__main__':
   app.run(debug=True)
--- model/models.py
+++ model/models.py
@@ -1,8 +1,9 @@
 # models.py
 from flask_sqlalchemy import SQLAlchemy
+from flask_marshmallow import Marshmallow
 
 db = SQLAlchemy()
-
+ma = Marshmallow()
 class Tutor(db.Model):
   id = db.Column(db.Integer, primary_key=True)
   nome = db.Column(db.String(100))
@@ -12,3 +13,11 @@ class Pet(db.Model):
   id = db.Column(db.Integer, primary_key=True)
   nome = db.Column(db.String(100))
   tutor_id = db.Column(db.Integer, db.ForeignKey('tutor.id'), nullable=False)
+  
+class TutorSchema(ma.Schema):
+  class Meta:
+      fields = ('id', 'nome', 'pets')
+      
+class PetSchema(ma.Schema):
+  class Meta:
+    fields = ('id', 'nome', 'tutor_id')
